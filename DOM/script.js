25c104ee7e310e16c6b7ce46ede04280d490058e
Muhammad Hanan Baloch <106446229+muhammadhananbaloch@users.noreply.github.com>
2023-09-17 13:37:12 +0500

    Add blue h3 element using javascript and DOM methods only.

--- DOM/script.js
+++ DOM/script.js
@@ -8,9 +8,18 @@ container.appendChild(content);
 
 // a <p> with red text that says “Hey I’m red!”
 
-const redText = document.createElement('p');;
+const redText = document.createElement('p');
 redText.classList.add('red-text');
 redText.style.color = 'red';
 redText.textContent = "Hey I'm red!" 
 
 container.appendChild(redText)
+
+// an <h3> with blue text that says “I’m a blue h3!”
+
+const blueHeader = document.createElement('h3');
+blueHeader.classList.add('blue-text');
+blueHeader.style.color = 'blue';
+blueHeader.textContent = "I'm a blue h3";
+
+container.appendChild(blueHeader);
