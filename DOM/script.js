9cfe3292974f6b65209951258281b53980726589
Muhammad Hanan Baloch <106446229+muhammadhananbaloch@users.noreply.github.com>
2023-09-17 13:34:01 +0500

    Add red text using javascript and DOM methods only.

--- DOM/script.js
+++ DOM/script.js
@@ -5,3 +5,12 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+// a <p> with red text that says “Hey I’m red!”
+
+const redText = document.createElement('p');;
+redText.classList.add('red-text');
+redText.style.color = 'red';
+redText.textContent = "Hey I'm red!" 
+
+container.appendChild(redText)
